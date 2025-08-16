52a30e2a5890565f00fd2390b63696097b70dba9
dandyphuong99 <dandyphuong99@gmail.com>
2025-08-16 16:56:59 +0700

    Change model

--- database/migrations/create_seller_payment_apis_table.php
+++ database/migrations/create_seller_payment_apis_table.php
@@ -8,8 +8,9 @@ return new class extends Migration {
     {
         Schema::create('seller_payment_apis', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('seller_extra_id')->constrained('seller_extra_infos')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('provider_id')->constrained('payment_providers')->cascadeOnDelete();
+            $table->foreignId('payment_method_id')->constrained('payment_methods')->cascadeOnDelete();
             $table->string('api_key');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
--- src/Models/SellerPaymentApi.php
+++ src/Models/SellerPaymentApi.php
@@ -2,26 +2,33 @@
 
 namespace Dandyphuong99\PaymentGateway\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SellerPaymentApi extends Model
 {
     protected $fillable = [
-        'seller_extra_id',
+        'user_id',
         'provider_id',
+        'payment_method_id',
         'api_key',
         'is_active',
     ];
 
-    // public function seller(): BelongsTo
-    // {
-    //     return $this->belongsTo(\App\Models\SellerExtraInfo::class, 'seller_extra_id');
-    // }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     public function provider(): BelongsTo
     {
         return $this->belongsTo(PaymentProvider::class, 'provider_id');
     }
+
+    // public function paymentMethod(): BelongsTo
+    // {
+    //     return $this->belongsTo(PaymentMethod::class, 'payment_method_id');
+    // }
 
 }
